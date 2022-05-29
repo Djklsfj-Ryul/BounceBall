81a65101db99d82eac384c7b362c76ced5fe8842
Your Name <you@example.com>
2022-05-30 00:36:28 +0900

    1주차
    해야할 일
    1: 충돌 시 생기는 약간의 정지 상태 해결
    2: 이단 점프, 대포 등등의 특수 기능 구현
    3: 배경, 스테이지 선택, 애니메이션 구현

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    int Player = 3;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * 5 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 5 * Time.deltaTime);
+        }
+    }
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    float bounce = 15f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigid.velocity = Vector2.zero;
+        Vector2 Jump = new Vector2(0, bounce);
+
+        if (collision.gameObject.tag == "WALL")
+        {
+            rigid.AddForce(Jump, ForceMode2D.Impulse);
+        }
+        if (collision.gameObject.tag == "TARGET")
+        {
+            Player--;
+            Debug.Log(Player);
+            if (Player == 0)
+            {
+                Debug.Log("GAME CLEAR");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
